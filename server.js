d524573f4fff296136b234355d69d6828788cf10
Ranveer <ranveer@Aditis-MacBook-Air.local>
2023-06-08 22:09:19 +0530

    first

--- server.js
+++ server.js
@@ -14,7 +14,9 @@ app.post('/events', (req, res) => {
     let callStatus = req.body.CallStatus;
     let callSid = req.body.callSid;
   
+ 
   console.log(to, fromNumber, callStatus, callSid);
+  console.log(req.body)
     res.send('Event received');
   });
 
@@ -22,13 +24,26 @@ app.post('/voice', (req, res) => {
     console.log("got pinged")
   // Generate a TwiML response
   let twiml = new twilio.twiml.VoiceResponse();
-  // Talk in a robot voice over the phone.
-  twiml.say('Call progress events are rad');
-  // Set the response type as XML.
-  res.header('Content-Type', 'text/xml');
+
+  twiml.gather({
+    input: 'speech',
+    timeout: 3,
+    hints: ['cat', 'hello'],
+    action: 'https://48b9-2401-4900-4456-9bff-8ba-384-58aa-ad30.ngrok-free.app/events'
+  }).say({
+    voice: 'Polly.Raveena',
+    language: 'en-IN'
+}, 'Welcome to speech demo, please say something')
+  
+
+  // If the user doesn't enter input, loop
+  twiml.redirect('/voice');
+
+  // Render the response as XML in reply to the webhook request
+  response.type('text/xml');
   // Send the TwiML as the response.
-  res.send(twiml.toString());
-});
+  res.send(twilio.toString())
+})
 
 // Run server to listen on port 3000.
 const server = app.listen(3001, () => {
