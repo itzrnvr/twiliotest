0659f5953638df9a685a3fdf5784a0eef30ecef7
Ranveer <ranveer@Aditis-MacBook-Air.local>
2023-06-09 06:21:08 +0530

    speech recog acheived

--- makeCall.js
+++ makeCall.js
@@ -1,19 +1,17 @@
-const accountSid = "AC0e6fbce2ba56bf5e7a679f444dfeca89";
-const authToken = "e61da247ab9e5d6af8bc2936d251af72";
+require('dotenv').config()
+
+const accountSid = process.env.SID;
+const authToken = process.env.TOKEN;
 const client = require('twilio')(accountSid, authToken);
 
-const ngrok = 'https://48b9-2401-4900-4456-9bff-8ba-384-58aa-ad30.ngrok-free.app'
 
-client.calls
-    .create({
-        method: 'POST',
-        url: `https://48b9-2401-4900-4456-9bff-8ba-384-58aa-ad30.ngrok-free.app/voice`,
-        to: '+917091447295',
-        from: '+13614701971',
-        statusCallback: 'https://48b9-2401-4900-4456-9bff-8ba-384-58aa-ad30.ngrok-free.app/events',
-        statusCallbackMethod: 'POST',
-        statusCallbackEvent: ['initiated', 'ringing', 'answered', 'completed'],
-    })
-    .then(call => console.log(call.sid));
-
+client.calls.create({
+    url: `${process.env.BASEURL}/action/digit`,
+    method: 'GET',
+    to: '+917091447295',
+    from: '+13614701971',
+    statusCallback: `${process.env.BASEURL}/events`,
+    statusCallbackMethod: 'POST',
+    statusCallbackEvent: ['initiated', 'ringing', 'answered', 'completed'],
+}).then(call => console.log(call.sid))
     
--- server.js
+++ server.js
@@ -1,4 +1,6 @@
-"use strict";
+require('dotenv').config()
+
+const BASEURL = process.env.BASEURL
 
 const express = require('express');
 const bodyParser = require('body-parser');
@@ -7,45 +9,47 @@ const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false } ));
 
-// Set Express routes.
-app.post('/events', (req, res) => {
-    let to = req.body.to;
-    let fromNumber = req.body.from;
-    let callStatus = req.body.CallStatus;
-    let callSid = req.body.callSid;
-  
- 
-  console.log(to, fromNumber, callStatus, callSid);
-  console.log(req.body)
-    res.send('Event received');
+app.get('/action/digit', (req, res) => {
+  console.log("PINged")
+  const twiml = new twilio.twiml.VoiceResponse();
+  const gather = twiml.gather({
+    action: `${process.env.BASEURL}/action/digit`,
+    input: 'speech',
+    timeout: 4,
+    hints: ['cat', 'hello'],
+    method: 'POST'
   });
 
-app.post('/voice', (req, res) => {
-    console.log("got pinged")
-  // Generate a TwiML response
-  let twiml = new twilio.twiml.VoiceResponse();
+  gather.say("Hello user, please say CAT");
 
-  twiml.gather({
-    input: 'speech',
-    timeout: 3,
-    hints: ['cat', 'hello'],
-    action: 'https://48b9-2401-4900-4456-9bff-8ba-384-58aa-ad30.ngrok-free.app/events'
-  }).say({
-    voice: 'Polly.Raveena',
-    language: 'en-IN'
-}, 'Welcome to speech demo, please say something')
-  
+  twiml.redirect({
+    method: 'GET'
+  }, `${process.env.BASEURL}/action/digit`);
 
-  // If the user doesn't enter input, loop
-  twiml.redirect('/voice');
+  res.set(`Content-Type`, `text/xml`);
 
-  // Render the response as XML in reply to the webhook request
-  response.type('text/xml');
-  // Send the TwiML as the response.
-  res.send(twilio.toString())
+  res.send(twiml.toString());
 })
 
-// Run server to listen on port 3000.
-const server = app.listen(3001, () => {
-  console.log('listening on *:3001');
+
+app.post('/action/digit', (req, res) => {
+  console.log(req.body)
+  console.log(req.body.SpeechResult)
+  const twiml = new twilio.twiml.VoiceResponse();
+  twiml.say({ voice:`alice` }, `Thank you for your feedback.`);
+  res.type(`text/xml`);
+  res.send(twiml.toString());
+})
+
+app.post('/events', (req, res) => {
+  let callStatus = req.body.CallStatus;
+  
+  console.log(callStatus);
+
+  res.send('Event received');
+});
+
+
+const server = app.listen('3000', () => {
+  console.log(`listening on *:${process.env.PORT}`);
 });
